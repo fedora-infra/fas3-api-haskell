59e7fea47021a36c26feab99831443ae50106188
Ricky Elrod <ricky@elrod.me>
2015-01-15 13:16:42 -0500

    move this to be under fedora-infra
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- scripts/deploy-haddock-manually.sh
+++ scripts/deploy-haddock-manually.sh
@@ -3,21 +3,21 @@
 cwd="$( cd "${BASH_SOURCE[0]%/*}" && pwd )"
 cd "$cwd/.."
 f=`mktemp -d`
-git clone "git@github.com:relrod/fas3.git" "$f/fas3.git"
+git clone "git@github.com:fedora-infra/fas3-api-haskell.git" "$f/fas3-api-haskell.git"
 cabal haddock
-pushd "$f/fas3.git"
+pushd "$f/fas3-api-haskell.git"
   git checkout gh-pages && git rm -rf *
 popd
-mv dist/doc/html/fas3/* "$f/fas3.git/"
-pushd "$f/fas3.git"
+mv dist/doc/html/fas3/* "$f/fas3-api-haskell.git/"
+pushd "$f/fas3-api-haskell.git"
   git add -A
-  git commit -m "Manual docs deploy."
+  git commit -m "Deploy haddock docs to github-pages."
   git push origin gh-pages
 popd
 rm -rf "$f"
 
 if [ $? == 0 ]; then
-  echo "*** Done: http://relrod.github.io/fas3/"
+  echo "*** Done: https://fedora-infra.github.io/fas3-api-haskell/"
   exit 0
 else
   echo "*** ERROR!!! Fix the above and try again."
