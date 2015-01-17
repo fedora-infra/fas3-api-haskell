3bea82d1cd2cd7332f75eef269efe55e6686f1ad
Ricky Elrod <ricky@elrod.me>
2015-01-16 21:40:02 -0500

    Make 'listPeople' actually do something.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Fedora/FAS/Client.hs
+++ src/Fedora/FAS/Client.hs
@@ -2,20 +2,25 @@
 module Fedora.FAS.Client where
 
 import Control.Lens
-import Data.Aeson
-import qualified Data.ByteString.Lazy.Char8 as C8
+--import Data.Aeson
+import qualified Data.ByteString.Char8 as C8
+import qualified Data.ByteString.Lazy.Char8 as LC8
 import Fedora.FAS.Types
+import Network.HTTP.Types (urlEncode)
 import Network.Wreq
---import Network.Wreq.Session
 
 localClientConfig :: APIKey -> ClientConfig
 localClientConfig = ClientConfig "http://localhost:6543"
 
+-- TODO: This is inefficient.
+encodePath :: String -> String
+encodePath = C8.unpack . urlEncode False . C8.pack
+
 listPeople :: ClientConfig -- ^ How to connect to FAS3
            -> SearchType -- ^ What to filter results by
            -> String -- ^ The search query
-           -> IO C8.ByteString -- TODO
+           -> IO LC8.ByteString -- TODO
 listPeople (ClientConfig b a) search query = do
   let opts = defaults & param "apikey" .~ [a]
-  r <- getWith opts b
+  r <- getWith opts (b ++ "/api/people/" ++ show search ++ "/" ++ encodePath query)
   return $ r ^. responseBody
