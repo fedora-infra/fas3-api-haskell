f608b0344bfd6fc7a8c5356cd8361fd5ebdd9076
Ricky Elrod <ricky@elrod.me>
2015-01-17 21:03:54 -0500

    Fix haddock
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Fedora/FAS/Client.hs
+++ src/Fedora/FAS/Client.hs
@@ -18,7 +18,7 @@ encodePath = C8.unpack . urlEncode False . C8.pack
 
 -- | Finds a unique person by some unique identifier ('SearchType').
 --
--- Internally, this hits @/api/people/<searchtype>/<query>@.
+-- Internally, this hits @\/api\/people\/<searchtype>\/<query>@.
 getPerson :: ClientConfig -- ^ How to connect to FAS3
           -> SearchType -- ^ What to filter results by
           -> String -- ^ The search query
@@ -30,7 +30,7 @@ getPerson (ClientConfig b a) search query = do
 
 -- | Get a list of all people.
 --
--- Internally, this hits @/api/people@.
+-- Internally, this hits @\/api\/people@.
 getPeople :: ClientConfig -- ^ How to connect to FAS3
           -> Integer -- ^ The page number
           -> Integer -- ^ The limit
