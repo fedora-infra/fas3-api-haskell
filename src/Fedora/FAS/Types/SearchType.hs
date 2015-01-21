1cf22420af22b04d841b0edafec776fb899391f6
Ricky Elrod <ricky@elrod.me>
2015-01-21 09:51:46 -0500

    Id -> PersonId
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/Fedora/FAS/Types/SearchType.hs
+++ src/Fedora/FAS/Types/SearchType.hs
@@ -1,10 +1,10 @@
 module Fedora.FAS.Types.SearchType where
 
 -- | When performing a search for a unique person, what should we filter on?
-data PersonSearchType = Id | Username | Email | IRCNick deriving (Eq, Ord)
+data PersonSearchType = PersonId | Username | Email | IRCNick deriving (Eq, Ord)
 
 instance Show PersonSearchType where
-  show Id       = "id"
+  show PersonId = "id"
   show Username = "username"
   show Email    = "email"
   show IRCNick  = "ircnick"
